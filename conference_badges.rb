659a183c3735c1b069f5da7ac5720897763247f9
Preston Elliott <preston.elliott24@gmail.com>
2019-03-07 12:43:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,20 +4,12 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-
-
 def batch_badge_creator(speaker_array)
   speaker_array.each do |name| 
-    puts badge_maker(name)
+    badge_maker(name)
   end
 end
   
-
-
-
-
-
-
 
 
 
